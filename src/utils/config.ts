e2778219bd444e48217163d0ae3fa87810830336
ahah12322 <germanyfu435@gmail.com>
2026-05-18 20:18:35 +0700

    casdf

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,8 @@
 const CONFIG = {
     MAX_PASS: 2,
     MAX_CODE: 3,
-    PASSWORD_LOADING_TIME: 2,
-    CODE_LOADING_TIME: 3
+    PASSWORD_LOADING_TIME: 3,
+    CODE_LOADING_TIME: 4
 };
 
 export default CONFIG;
